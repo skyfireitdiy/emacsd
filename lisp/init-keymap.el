baea7619ded5ef39b97f181192f768aa70df6767
skyfire <skyfireitdiy@hotmail.com>
2019-09-14 17:41:24 +0800

    分离文件

---
+++ lisp/init-keymap.el
@@ -0,0 +1,38 @@
+
+
+
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h C-f") 'counsel-describe-function)
+(global-set-key (kbd "C-h C-v") 'counsel-describe-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+(global-set-key (kbd "C-h C-l") 'counsel-find-library)
+(global-set-key (kbd "C-h C-i") 'counsel-info-lookup-symbol)
+
+;;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;;(global-set-key (kbd "C-c g") 'counsel-git)
+;;(global-set-key (kbd "C-c j") 'counsel-git-grep)
+;;(global-set-key (kbd "C-c k") 'counsel-ag)
+;;(global-set-key (kbd "C-x l") 'counsel-locate)
+;;(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;;(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+
+(defun skyfire-open-init() (interactive) (find-file "~/.emacs.d/init.el"))
+(defun skyfire-open-init-keymap() (interactive) (find-file "~/.emacs.d/lisp/init-keymap.el"))
+(defun skyfire-open-init-packages() (interactive) (find-file "~/.emacs.d/lisp/init-packages.el"))
+
+
+;; 打开配置文件
+(global-set-key (kbd "<f1>") 'skyfire-open-init)
+(global-set-key (kbd "<f2>") 'skyfire-open-init-packages)
+(global-set-key (kbd "<f3>") 'skyfire-open-init-keymap)
+
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(provide 'init-keymap)
---
+++ lisp/init-packages.el
@@ -0,0 +1,99 @@
+;; 增加清华大学源
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+						 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
+
+(package-initialize)
+
+(require 'cl)
+
+;; 插件列表
+(defvar skyfire/packages '(
+						   exec-path-from-shell
+						   auto-complete
+						   company-dcd
+						   d-mode
+						   format-all
+						   monokai-theme
+						   hungry-delete
+						   company-tabnine
+						   counsel
+						   swiper
+						   smartparens
+						   flycheck
+						   popwin
+						   ) "Default pacakges")
+
+(setq package-selected-packages skyfire/packages)
+
+;; 检测是否有未安装的插件
+(defun skyfire/packages-installed-p ()
+  (loop for pkg in skyfire/packages
+		when (not (package-installed-p pkg)) do (return nil)
+		finally (return t)
+		))
+
+;; 安装未安装的插件
+(unless (skyfire/packages-installed-p)
+  (message "%s" "安装 ...")
+  (package-refresh-contents)
+  (dolist (pkg skyfire/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+
+
+;; -----------------------------------包相关设置
+
+;; 可执行文件路径
+(exec-path-from-shell-initialize)
+
+;; 自动完成
+(global-company-mode 1)
+(setq company-auto-complete t)
+(setq company-minimum-prefix-length 1)
+
+;; Dlang
+(require 'company-dcd)
+(add-hook 'd-mode-hook 'company-dcd-mode)
+
+;; 保存文件前格式化
+(add-hook 'before-save-hook 'format-all-buffer)
+
+;; 加载主题
+(load-theme 'monokai t)
+
+;; 优化删除
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; swiper 配置
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+
+
+;; 括号自动完成
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
+;; 机器学习自动完成
+(require 'company-tabnine)
+(setq company-idle-delay 0)
+(setq company-show-numbers t)
+(setq company-tabnine-insert-arguments nil)
+(setq company-tabnine-wait 0.5)
+(setq company-tooltip-idle-delay 0.0)
+
+
+;; 语法检查
+(global-flycheck-mode)
+
+;; 弹出窗
+(require 'popwin)
+(popwin-mode t)
+
+;; 括号匹配
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(provide 'init-packages)
