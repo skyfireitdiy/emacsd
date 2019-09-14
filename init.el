1fb96142d01b18a676fc23ca0f1eb28940ba816b
skyfire <skyfireitdiy@hotmail.com>
2019-09-14 15:53:58 +0800

    基本i配置

--- init.el
+++ init.el
@@ -1,31 +1,38 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comment.
-;; (package-initialize)
 
+;; 增加清华大学源
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+						 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
+
 (package-initialize)
 
-
-
-(require 'cl)
+;; 插件列表
 (defvar skyfire/packages '(
-			   exec-path-from-shell
-			   company
-			   auto-complete
-			   company-dcd
-			   d-mode
-			   format-all
-			   ) "Default pacakges")
+						   exec-path-from-shell
+						   auto-complete
+						   company-dcd
+						   d-mode
+						   format-all
+						   monokai-theme
+						   hungry-delete
+						   company-tabnine
+						   counsel
+						   swiper
+						   smartparens
+						   flycheck
+						   ) "Default pacakges")
 
+(setq package-selected-packages skyfire/packages)
+
+
+;; 检测是否有未安装的插件
 (defun skyfire/packages-installed-p ()
   (loop for pkg in skyfire/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)
-	))
+		when (not (package-installed-p pkg)) do (return nil)
+		finally (return t)
+		))
 
+;; 安装未安装的插件
 (unless (skyfire/packages-installed-p)
   (message "%s" "安装 ...")
   (package-refresh-contents)
@@ -33,13 +40,18 @@
     (when (not (package-installed-p pkg))
       (package-install pkg))))
 
+;; 机器学习自动完成
+(require 'company-tabnine)
+(setq company-idle-delay 0)
+(setq company-show-numbers t)
+
+;; 语法检查
+(global-flycheck-mode)
+
 ;; -----------------------------------包相关设置
 
 ;; 可执行文件路径
 (exec-path-from-shell-initialize)
-
-;; 自动完成
-(ac-config-default)
 
 ;; Dlang
 (require 'company-dcd)
@@ -48,12 +60,42 @@
 ;; 保存文件前格式化
 (add-hook 'before-save-hook 'format-all-buffer)
 
-;; --------------------------------------------
+;; 加载主题
+(load-theme 'monokai t)
+
+;; 优化删除
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; swiper 配置
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 
+;; 括号自动完成
+(require 'smartparens-config)
+(smartparens-global-mode t)
 
-
-
+;; -------------------------------------------
 
 ;; 关闭工具栏
 (tool-bar-mode -1)
@@ -76,7 +118,7 @@
   (interactive)
   (find-file "~/.emacs.d/init.el")
   )
-(global-set-key (kbd "<f2>") 'open-my-el-init)
+(global-set-key (kbd "<f3>") 'open-my-el-init)
 
 ;; 历史文件
 (require 'recentf)
@@ -94,14 +136,16 @@
 (global-hl-line-mode t)
 
 ;; tab宽度
+(setq-default tab-width 4)
 (setq tab-width 4)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
